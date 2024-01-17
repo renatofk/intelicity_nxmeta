0d8be8114eecb1d51af7102ca07cf6a932a5a62b
Fostertech <renato@fostertech.me>
2024-01-17 11:47:10 -0300

    try local url from railway

--- services/index.js
+++ services/index.js
@@ -2,7 +2,7 @@
 
 export const register_user = async (formData) => {
     try {
-        const res = await fetch('https://intelicitynxmeta-production.up.railway.app/api/Auth/register', {
+        const res = await fetch('http://localhost:5568/api/Auth/register', {
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -20,7 +20,7 @@ export const register_user = async (formData) => {
 
 export const login_user = async (formData) => {
     try {
-        const res = await fetch('https://intelicitynxmeta-production.up.railway.app/api/Auth/login', {
+        const res = await fetch('http://localhost:5568/api/Auth/login', {
             headers: {
                 'Content-Type': 'application/json',
             },
